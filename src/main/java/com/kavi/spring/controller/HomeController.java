fbd4e79b9ea9dbba5d9adc4fe017df8ffe1de28d
Ela <elamaran.annamalai@contractor.cengage.com>
2017-01-09 00:58:40 -0500

    initial commit

--- src/main/java/com/kavi/spring/controller/HomeController.java
+++ src/main/java/com/kavi/spring/controller/HomeController.java
@@ -3,16 +3,12 @@ package com.kavi.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by eannamal on 1/9/17.
- */
-
 @Controller
-@RequestMapping("/")
+@RequestMapping({"/", "/home"})
 public class HomeController {
 
     @RequestMapping
-    public String login() {
-        return "login";
+    public String home() {
+        return "home";
     }
 }
